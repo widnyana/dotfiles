cbba89a7398e7ff2946ae2020231c98455a42473
widnyana <wid@widnyana.web.id>
2020-07-17 14:57:04 +0700

    feat: add new function for docker
    Signed-off-by: widnyana <wid@widnyana.web.id>

--- functions.sh
+++ functions.sh
@@ -9,3 +9,18 @@ gh_latest_release() {
       sed -E 's/.*"([^"]+)".*/\1/'                                    # Pluck JSON value
   fi
 }
+
+
+removecontainers() {
+    docker stop $(docker ps -aq)
+    docker rm $(docker ps -aq)
+}
+
+armageddon() {
+    removecontainers
+    docker network prune -f
+    docker rmi -f $(docker images --filter dangling=true -qa)
+    docker volume rm $(docker volume ls --filter dangling=true -q)
+    docker rmi -f $(docker images -qa)
+}
+

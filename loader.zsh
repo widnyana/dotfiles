cf697822add89bbe48279ea267a8869258f308fd
widnyana <wid@widnyana.web.id>
2023-05-06 17:52:57 +0700

    add docker-compose installer
    Signed-off-by: widnyana <wid@widnyana.web.id>

--- loader.zsh
+++ loader.zsh
@@ -2,34 +2,49 @@
 _DOT_DIR=$0:a:h
 
 
-source ${_DOT_DIR}/aliases
-source ${_DOT_DIR}/python
-source ${_DOT_DIR}/nodejs
-source ${_DOT_DIR}/golang
-source ${_DOT_DIR}/paths
-source ${_DOT_DIR}/devs
-source ${_DOT_DIR}/android
+create_auth_keys() {
+  cat <<EOF > "${_DOT_DIR}/auth_keys" 
+GITHUB_PERSONAL_TOKEN=
+EOF
+}
 
-if [[ -x "$(command -v foo)" ]]; then
-  eval "$(direnv hook zsh)"
+
+if [ ! -f ${_DOT_DIR}/auth_keys ]; then
+  create_auth_keys
+  echo "please configure your token here: ${_DOT_DIR}/auth_keys"
+else
+  source ${_DOT_DIR}/auth_keys
+
+
+  source ${_DOT_DIR}/functions.sh
+  source ${_DOT_DIR}/aliases
+  source ${_DOT_DIR}/python
+  source ${_DOT_DIR}/nodejs
+  source ${_DOT_DIR}/golang
+  source ${_DOT_DIR}/paths
+  source ${_DOT_DIR}/devs
+  source ${_DOT_DIR}/android
+
+  if [[ -x "$(command -v direnv)" ]]; then
+    eval "$(direnv hook zsh)"
+  fi
+
+  #: load ssh alias
+  for box in ${HOME}/.dotfiles/boxes/*.box; do
+      source "${box}"
+  done 
+
+  #: specific per OS
+  if [[ $(uname) == "Linux" ]]; then
+
+  elif [[ $(uname) == "Darwin" ]]; then
+      source ${_DOT_DIR}/mac
+  fi
+
+  #: finalize $PATH
+  FINAL_PATH=$(python ${_DOT_DIR}/bin/_pathnodupe.py)
+  export PATH=${FINAL_PATH}
+
+  #: clean up
+  unset _BOXES _DOT_DIR FINAL_PATH
 fi
-
-
-#: load ssh alias
-for box in ${HOME}/.dotfiles/boxes/*.box; do
-    source "${box}"
-done 
-
-#: specific per OS
-if [[ $(uname) == "Linux" ]]; then
-
-elif [[ $(uname) == "Darwin" ]]; then
-    source ${_DOT_DIR}/mac
-fi
-
-#: finalize $PATH
-FINAL_PATH=$(python ${_DOT_DIR}/bin/_pathnodupe.py)
-export PATH=${FINAL_PATH}
-
-#: clean up
-unset _BOXES _DOT_DIR FINAL_PATH

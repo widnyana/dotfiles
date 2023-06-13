0c3b598649b0b26f1e632aad235ef1a0bf1b80d0
widnyana <wid@widnyana.web.id>
2023-06-13 12:04:22 +0700

    feat(tmux): patch broken oh-my-tmux
    Signed-off-by: widnyana <wid@widnyana.web.id>

--- bin/install-dotfiles.sh
+++ bin/install-dotfiles.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
+set -euo pipefail
+
 CONFIG_DIR="${XDG_CONFIG_HOME:-$HOME/.config}"
 DOT_DIR="${HOME}/.dotfiles"
+
+pushd $DOT_DIR
 
 ### pull the submodules
 git submodule update --init --recursive --rebase -f
@@ -13,11 +17,16 @@ ln -sfn "${HOME}/.dotfiles/git/gitignore"   "${HOME}/.gitignore"
 
 ### Tmux
 mkdir -p    "${CONFIG_DIR}/tmux"
-mkdir -p    "${HOME}/.tmux/plugins/"
+mkdir -p    "${CONFIG_DIR}/tmux/plugins/"
 ln -sn -f   "${DOT_DIR}/submodule/tmux/.tmux.conf"      "${CONFIG_DIR}/tmux/tmux.conf"
 ln -sn -f   "${DOT_DIR}/config/tmux/tmux.conf.local"    "${CONFIG_DIR}/tmux/tmux.conf.local"
 
 
+if [[ ! -d "${CONFIG_DIR}/tmux/plugins/tpm" ]]; then
+    echo -e "tpm does not exist"
+    git clone https://github.com/tmux-plugins/tpm ${CONFIG_DIR}/tmux/plugins/tpm
+    ${CONFIG_DIR}/tmux/plugins/tpm/bin/install_plugins
+fi
 
 
 ### VIM
@@ -27,3 +36,5 @@ ln -sfn "${HOME}/.dotfiles/vim/vimrc"       "${HOME}/.vimrc"
 mkdir -p        "${CONFIG_DIR}/alacritty"
 mkdir -p        "${CONFIG_DIR}/alacritty/themes"
 ln    -sfn      "${DOT_DIR}/config/alacritty/alacritty.yml"         "${CONFIG_DIR}/alacritty/alacritty.yml"
+
+popd

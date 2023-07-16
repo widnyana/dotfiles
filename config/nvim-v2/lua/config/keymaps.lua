5d02a2a0c25502b9d33cbd4e4fbf127f3ae87956
widnyana <wid@widnyana.web.id>
2023-07-16 18:09:37 +0700

    feat(nvim): update schemastore, utils, keymaps, nvim-tree, icons, options
    Signed-off-by: widnyana <wid@widnyana.web.id>

--- config/nvim-v2/lua/config/keymaps.lua
+++ config/nvim-v2/lua/config/keymaps.lua
@@ -1,9 +1,30 @@
-local utils = require("utils")
+local keymap = require('utils').keymap
+
 
 -- Remap command key
-vim.keymap.set("n", "<leader><leader>", ":")
-vim.keymap.set("n", "<C-p>", ":")
+keymap("n", "<leader><leader>", ":")
+keymap("n", "<C-p>", ":")
 
+
+-- buffer navigation
+keymap('n', '<leader>bp', ':bprev<cr>', { desc = 'Prev buffer' })
+keymap('n', '<leader>bn', ':bnext<cr>', { desc = 'Next buffer' })
+keymap('n', '<leader>bd', ':bdelete<cr>', { desc = 'Delete buffer' })
+
+-- tab navigation
+keymap('n', '<leader>tp', ':tabprevious<cr>', { desc = 'Prev tab' })
+keymap('n', '<leader>tn', ':tabnext<cr>', { desc = 'Next tab' })
+keymap('n', '<leader>td', ':tabclose<cr>', { desc = 'Close tab' })
+
+-- plugin management
+keymap('n', '<leader>pc', ':Lazy check<cr>', { desc = 'Check plugins' })
+keymap('n', '<leader>pu', ':Lazy update<cr>', { desc = 'Update plugins' })
+keymap('n', '<leader>ps', ':Lazy show<cr>', { desc = 'Show plugins' })
+keymap('n', '<leader>ph', ':Lazy help<cr>', { desc = 'Help' })
+keymap('n', '<leader>pp', ':Lazy profile<cr>', { desc = 'Profile' })
+keymap('n', '<leader>pl', ':Lazy logs<cr>', { desc = 'Logs' })
+keymap('n', '<leader>px', ':Lazy clear<cr>', { desc = 'Clear uninstalled plugins' })
+keymap('n', '<leader>pr', ':Lazy restore<cr>', { desc = 'Restore plugins from lockfile' })
 
 
 -- Nvim-Tree

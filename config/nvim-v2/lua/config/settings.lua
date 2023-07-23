858f85594f01b1e1bcaa26dfbf10b72548f8df26
widnyana <wid@widnyana.web.id>
2023-07-23 13:45:02 +0700

    feat: update neovim config
    Signed-off-by: widnyana <wid@widnyana.web.id>

--- config/nvim-v2/lua/config/settings.lua
+++ config/nvim-v2/lua/config/settings.lua
@@ -229,8 +229,73 @@ M.telescope_file_ignore_patterns = {
     "vendor/*",
 }
 
+-- list of disabled plugins
 M.disable_builtin_plugins = {
-    
+
 }
+
+
+-- stolen from NvChad core.mappings
+M.mappings = {
+    general = {
+        i = {
+            -- go to  beginning and end
+            ["<C-b>"] = { "<ESC>^i", "Beginning of line" },
+            ["<C-e>"] = { "<End>", "End of line" },
+
+            -- navigate within insert mode
+            ["<C-h>"] = { "<Left>", "Move left" },
+            ["<C-l>"] = { "<Right>", "Move right" },
+            ["<C-j>"] = { "<Down>", "Move down" },
+            ["<C-k>"] = { "<Up>", "Move up" },
+        },
+
+        n = {
+            ["<Esc>"] = { ":noh <CR>", "Clear highlights" },
+            -- switch between windows
+            ["<C-h>"] = { "<C-w>h", "Window left" },
+            ["<C-l>"] = { "<C-w>l", "Window right" },
+            ["<C-j>"] = { "<C-w>j", "Window down" },
+            ["<C-k>"] = { "<C-w>k", "Window up" },
+        
+            -- save
+            ["<C-s>"] = { "<cmd> w <CR>", "Save file" },
+        
+            -- Copy all
+            ["<C-c>"] = { "<cmd> %y+ <CR>", "Copy whole file" },
+        
+            -- line numbers
+            ["<leader>n"] = { "<cmd> set nu! <CR>", "Toggle line number" },
+            ["<leader>rn"] = { "<cmd> set rnu! <CR>", "Toggle relative number" },
+        
+            -- Allow moving the cursor through wrapped lines with j, k, <Up> and <Down>
+            -- http://www.reddit.com/r/vim/comments/2k4cbr/problem_with_gj_and_gk/
+            -- empty mode is same as using <cmd> :map
+            -- also don't use g[j|k] when in operator pending mode, so it doesn't alter d, y or c behaviour
+            ["j"] = { 'v:count || mode(1)[0:1] == "no" ? "j" : "gj"', "Move down", opts = { expr = true } },
+            ["k"] = { 'v:count || mode(1)[0:1] == "no" ? "k" : "gk"', "Move up", opts = { expr = true } },
+            ["<Up>"] = { 'v:count || mode(1)[0:1] == "no" ? "k" : "gk"', "Move up", opts = { expr = true } },
+            ["<Down>"] = { 'v:count || mode(1)[0:1] == "no" ? "j" : "gj"', "Move down", opts = { expr = true } },
+        
+            -- new buffer
+            ["<leader>b"] = { "<cmd> enew <CR>", "New buffer" },
+            ["<leader>ch"] = { "<cmd> NvCheatsheet <CR>", "Mapping cheatsheet" },
+        
+            ["<leader>fm"] = {
+              function()
+                vim.lsp.buf.format { async = true }
+              end,
+              "LSP formatting",
+            },
+          },
+    }
+}
+
+
+-- theme for cheatsheet
+M.cheatsheet = {
+    theme = "grid"
+}
+
 
 return M

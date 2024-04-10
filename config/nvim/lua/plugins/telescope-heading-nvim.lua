8c090487849993a3e68ea912bb7c776283798c62
widnyana <wid@widnyana.web.id>
2024-04-10 21:24:40 +0700

    feat(nvim): init LazyVim
    Signed-off-by: widnyana <wid@widnyana.web.id>

--- config/nvim/lua/plugins/telescope-heading-nvim.lua
+++
@@ -1,6 +0,0 @@
--- An extension for telescope.nvim that allows you to switch between document's headings.
--- https://github.com/crispgm/telescope-heading.nvim
-
-return {
-    "crispgm/telescope-heading.nvim"
-}

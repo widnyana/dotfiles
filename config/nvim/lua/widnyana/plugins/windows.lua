c2ca03ba416ae2f97b395da2b07b144170d3004f
widnyana <wid@widnyana.web.id>
2026-07-18 09:58:58 +0700

    feat(nvim): add windows.nvim for automatic window maximizing
    Auto-maximize the active split on focus, with a manual
    <leader>m fallback.

---
+++ config/nvim/lua/widnyana/plugins/windows.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "anuvyklack/windows.nvim",
+    event = "WinNew",
+    dependencies = {
+      { "anuvyklack/middleclass" },
+      { "anuvyklack/animation.nvim", enabled = false },
+    },
+    keys = { { "<leader>m", "<cmd>WindowsMaximize<cr>", desc = "Maximize Window" } },
+    config = function()
+      vim.o.winwidth = 5
+      vim.o.equalalways = false
+      require("windows").setup({
+        animation = { enable = false, duration = 150 },
+      })
+    end,
+  },
+}
